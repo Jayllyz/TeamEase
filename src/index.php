d4e5146718b041a35c37d1f7bdbed28274435ad2
Antony David <antonydavid945@gmail.com>
2023-02-09 21:18:07 +0100

    add: show tables

--- src/index.php
+++ src/index.php
@@ -1,8 +1,18 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=mysql:3306;dbname=teamease','root','Respons11');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
+  $bdd = new PDO('mysql:host=mysql:3306;dbname=teamease', 'root', 'Respons11');
+  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo 'Connexion réussie !';
+  echo '<br>';
+  echo 'Liste des tables :';
+  echo '<br>';
+
+  $sql = 'SHOW TABLES';
+  $result = $bdd->query($sql);
+  while ($row = $result->fetch(PDO::FETCH_NUM)) {
+    echo $row[0] . '<br>';
   }
+} catch (PDOException $e) {
+  echo 'Erreur : ' . $e->getMessage();
+}
 ?>
