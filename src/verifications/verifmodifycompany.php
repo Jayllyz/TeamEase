59661675df19348f12e04146aff7ed651aede227
minatoco <102216647+minatoco@users.noreply.github.com>
2023-03-19 20:22:06 +0100

    add: conf email by phpmailer

--- src/verifications/verifmodifycompany.php
+++ src/verifications/verifmodifycompany.php
@@ -27,26 +27,73 @@ if (
   for ($i = 0; $i < count($compare); $i++) {
     if ($compare[$i]['email'] == $email && $compare[$i]['siret'] != $siret) {
       header('location: ../profile.php?message=Email déjà utilisé !&type=danger');
+      exit();
     }
     if ($compare[$i]['companyName'] == $companyName && $compare[$i]['siret'] != $siret) {
       header("location: ../profile.php?message=Nom d'entreprise déjà utilisé !&type=danger");
+      exit();
     }
     if ($compare[$i]['address'] == $address && $compare[$i]['siret'] != $siret) {
       header("location: ../profile.php?message=Adresse déjà utilisé !&type=danger");
+      exit();
     }
   }
 
-  exit();
-  $update = $db->prepare(
-    'UPDATE COMPANY SET email = :email, companyName = :companyName, rights = :rights, address = :address WHERE siret = :siret'
-  );
-  $update->execute([
-    'email' => $email,
-    'companyName' => $companyName,
-    'rights' => $rights,
-    'address' => $address,
+  $req = $db->prepare('SELECT email FROM COMPANY WHERE siret = :siret');
+  $req->execute([
     'siret' => $siret,
   ]);
-  header('location: ../profile.php?message=Modification effectué !&type=success');
-  exit();
+  $oldemail = $req->fetch();
+  $oldemail = $oldemail['email'];
+
+  if ($oldemail != $email) {
+
+    $token = uniqid();
+
+
+    $update = $db->prepare(
+      'UPDATE COMPANY SET email = :email, companyName = :companyName, rights = :rights, address = :address, token= :token, confirm_signup= :confirm_signup WHERE siret = :siret'
+    );
+    $update->execute([
+      'email' => $email,
+      'companyName' => $companyName,
+      'rights' => $rights,
+      'address' => $address,
+      'siret' => $siret,
+      'token' => $token,
+      'confirm_signup' => 0,
+
+    ]);
+
+      $subject = 'Confirmation de changement de mail';
+      $msgHTML =
+        '<img src="localhost/images/logo.png" class="logo float-left m-2 h-75 me-4" width="95" alt="Logo">
+                  <p class="display-2">Bienvenue chez Together&Stronger. Veuillez cliquer sur le lien ci-dessous pour confirmer votre changement de mail :<br></p>
+        <a href="localhost/includes/confemail.php?' .
+        'token=' .
+        $token .
+        '&email=' .
+        $email .
+        '&type=' .
+        'company">Confirmation changement de mail !</a>';
+      $destination = '../login.php';
+      include '../includes/mailer.php';
+
+  }else
+  {
+    $update = $db->prepare(
+      'UPDATE COMPANY SET email = :email, companyName = :companyName, rights = :rights, address = :address WHERE siret = :siret'
+    );
+    $update->execute([
+      'email' => $email,
+      'companyName' => $companyName,
+      'rights' => $rights,
+      'address' => $address,
+      'siret' => $siret,
+    ]);
+    header('location: ../profile.php?message=Modification effectuée !&type=success');
+    exit();
+  }
+
+
 }
