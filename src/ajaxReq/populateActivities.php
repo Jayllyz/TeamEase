cccb13ff18c6a0fe2a39b90a2dd06939167351fd
Jayllyz <Jayllyz@users.noreply.github.com>
2023-03-21 11:24:15 +0000

    Prettified Code!

--- src/ajaxReq/populateActivities.php
+++ src/ajaxReq/populateActivities.php
@@ -137,7 +137,7 @@ if ($countId == 0) {
               <p class="mb-0">';
     for ($j = 0; $j < $countCategory; $j++) {
       $query = $db->prepare(
-        'SELECT name, id FROM CATEGORY WHERE id IN (SELECT id_category FROM BELONG WHERE id_activity = :id_activity)'
+        'SELECT name, id FROM CATEGORY WHERE id IN (SELECT id_category FROM BELONG WHERE id_activity = :id_activity)',
       );
       $query->execute([':id_activity' => $id[$i]]);
       $categoryName = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -235,7 +235,7 @@ if ($countId == 0) {
               <p class="mb-0">';
     for ($j = 0; $j < $countCategory; $j++) {
       $query = $db->prepare(
-        'SELECT name, id FROM CATEGORY WHERE id IN (SELECT id_category FROM BELONG WHERE id_activity = :id_activity)'
+        'SELECT name, id FROM CATEGORY WHERE id IN (SELECT id_category FROM BELONG WHERE id_activity = :id_activity)',
       );
       $query->execute([':id_activity' => $id[$i]]);
       $categoryName = $query->fetchAll(PDO::FETCH_ASSOC);
