7dedb4826ff5e5cf4a6f5057ff9efd812c394401
Jayllyz <antonydavid945@gmail.com>
2023-03-21 10:38:03 +0100

    fix: reservations table

--- src/clients/reservations.php
+++ src/clients/reservations.php
@@ -33,9 +33,22 @@ $siret = $_SESSION['siret'];
         $req->execute([
           'siret' => $siret,
         ]);
-        $result = $req->fetchAll(PDO::FETCH_ASSOC);
+        $result = $req->fetchAll(PDO::FETCH_ASSOC);?>
 
-        foreach ($result as $select) { ?>
+
+        <div class="container info_user">
+        <table class="table text-center table-bordered table-hover">
+           <tr>
+            <th>Nom de l'activité</th>
+            <th>Date</th>
+            <th>Heure</th>
+            <th>Nombre de participants</th>
+            <th>Adresse et salle</th>
+            <th>Status</th>
+            <th>Action</th>
+           </tr>
+
+      <?php  foreach ($result as $select) { ?>
 
             <?php
             $date = explode('-', $select['date']);
@@ -50,68 +63,53 @@ $siret = $_SESSION['siret'];
             $id_room = $req->fetch(PDO::FETCH_ASSOC);
             ?>
 
-            <div class="container info_user">
-                <table class="table text-center table-bordered table-hover">
-                   <tr>
-                    <th>Nom de l'activité</th>
-                    <th>Date</th>
-                    <th>Heure</th>
-                    <th>Nombre de participants</th>
-                    <th>Adresse et salle</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                   </tr>
-
                    <tr>
                     <td><?php
-                    $req = $db->prepare('SELECT name FROM ACTIVITY WHERE id = :id');
-                    $req->execute([
-                      'id' => $select['id_activity'],
-                    ]);
-                    $result = $req->fetchAll(PDO::FETCH_ASSOC);
-                    foreach ($result as $name) {
-                      echo $name['name'];
-                    }
+                      $req = $db->prepare('SELECT name FROM ACTIVITY WHERE id = :id');
+                      $req->execute([
+                        'id' => $select['id_activity'],
+                      ]);
+                      $result = $req->fetchAll(PDO::FETCH_ASSOC);
+                      foreach ($result as $name) {
+                        echo $name['name'];
+                      }
                     ?></td>
                     <td><?= $date ?></td>
 
                     <td><?php
-                    $req = $db->prepare('SELECT duration FROM ACTIVITY WHERE id = :id');
-                    $req->execute([
-                      'id' => $select['id_activity'],
-                    ]);
+                        $req = $db->prepare('SELECT duration FROM ACTIVITY WHERE id = :id');
+                        $req->execute([
+                          'id' => $select['id_activity'],
+                        ]);
 
-                    $result = $req->fetch(PDO::FETCH_ASSOC);
-                    if ($result['duration'] > 60) {
-                      $hour = floor($result['duration'] / 60);
-                      $min = $result['duration'] % 60;
-                      $duration = $hour . 'h' . $min;
-                      echo $select['time'] . ' <br> ' . 'durée : ' . $duration;
-                    } else {
-                      echo $select['time'] . ' <br> ' . 'durée : ' . $result['duration'] . 'min';
-                    }
-                    ?></td>
+                        $result = $req->fetch(PDO::FETCH_ASSOC);
+                        if ($result['duration'] > 60) {
+                          $hour = floor($result['duration'] / 60);
+                          $min = $result['duration'] % 60;
+                          $duration = $hour . 'h' . $min;
+                          echo $select['time'] . ' <br> ' . 'durée : ' . $duration;
+                        } else {
+                          echo $select['time'] . ' <br> ' . 'durée : ' . $result['duration'] . 'min';
+                        }
+                      ?></td>
 
 
 
                     <td><?= $select['attendee'] ?></td>
                     <td><?
-                    $req = $db->prepare('SELECT id_location FROM ROOM WHERE id = :id');
-                    $req->execute([
-                      'id' => $id_room,
-                    ]);
-                    $result = $req->fetch(PDO::FETCH_ASSOC);
-                    
-                    $req = $db->prepare('SELECT address FROM LOCATION WHERE id = :id');
-                    $req->execute([
-                      'id' => $result['id_location'],
-                    ]);
-                    $result = $req->fetch(PDO::FETCH_ASSOC);
-                    echo 'Adresse : ' . '<br>' . $result['address'] . '<br>';
-                    echo 'Salle : ' . $select['id_room'];
-
-                    
-                    
+                      $req = $db->prepare('SELECT id, id_location FROM ROOM WHERE id = :id');
+                      $req->execute([
+                        'id' => $id_room['id_room'],
+                      ]);
+                      $resultRoom = $req->fetch(PDO::FETCH_ASSOC);
+                      
+                      $req = $db->prepare('SELECT address FROM LOCATION WHERE id = :id');
+                      $req->execute([
+                        'id' => $resultRoom['id_location'],
+                      ]);
+                      $result = $req->fetch(PDO::FETCH_ASSOC);
+                      echo 'Adresse : ' . '<br>' . $result['address'] . '<br>';
+                      echo 'Salle : ' . $resultRoom['id'];
                     ?></td>
                     <td><?php if ($select['status'] == 0) {
                       echo 'Pas encore réglée <svg width="46" height="46" fill="none" stroke="#df1111" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -122,16 +120,20 @@ $siret = $_SESSION['siret'];
                       echo 'Réservation réglée';
                     } ?></td>
                     <td><?php if ($select['status'] == 0) { ?>
-
-
-                        <?php
-                        $req = $db->prepare('SELECT priceAttendee, name FROM ACTIVITY WHERE id = :id');
-                        $req->execute([
-                          'id' => $select['id_activity'],
-                        ]);
-                        $result = $req->fetch(PDO::FETCH_ASSOC);
-                        $price = $result['priceAttendee'] * $select['attendee'];
-                        ?>
+                          <?php
+                          $req = $db->prepare('SELECT name FROM ACTIVITY WHERE id = :id');
+                          $req->execute([
+                            'id' => $select['id_activity'],
+                          ]);
+                          $name = $req->fetch(PDO::FETCH_ASSOC);
+                          
+                          $req = $db->prepare('SELECT amount FROM ESTIMATE WHERE id_reservation = :id');
+                          $req->execute([
+                            'id' => $select['id'],
+                          ]);
+                          $price = $req->fetch(PDO::FETCH_ASSOC);
+                          $price = $price['amount'] * $select['attendee'];
+                          ?>
 
                         <div class="button_profil">
                             <form action="checkPayment.php?id=<?= $select['id'] ?>" class="mb-4" method="POST">
@@ -139,7 +141,7 @@ $siret = $_SESSION['siret'];
                                         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
                                         data-key="<?= $stripePublicKey ?>"
                                         data-amount="<?= $price * 100 ?>"
-                                        data-name="<?= $result['name'] ?>"
+                                        data-name="<?= $name['name'] ?>"
                                         data-description="Paiement de la réservation"
                                         data-image="../images/logo.png"
                                         data-locale="auto"
@@ -156,11 +158,10 @@ $siret = $_SESSION['siret'];
 </svg>';} ?>
                     </td>
                    </tr>
-                </table>
-            </div>
-           
         <?php }
         ?>
+                     </table>
+            </div>
     </main>
     <?php include '../includes/footer.php'; ?>
 </body>
