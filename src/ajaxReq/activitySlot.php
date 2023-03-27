04c1bd8267e298c3702f4af85c5dc341a9947f95
Jayllyz <antonydavid945@gmail.com>
2023-03-27 15:24:36 +0200

    fix: reserv room verif

--- src/ajaxReq/activitySlot.php
+++ src/ajaxReq/activitySlot.php
@@ -83,9 +83,20 @@ if (isset($_POST['id']) && isset($_POST['date']) && isset($_POST['day'])) {
 
       if (is_array($reponse) && !empty($reponse)) {
         if ($attendee + $reponse['attendee'] <= $slot['maxAttendee'] && $reponse['siret'] != $_SESSION['siret']) {
+
+          if(empty($checkRoom)){
+            echo '<option value=' .
+              $startSlotArray[$j] .
+              '>' .
+              $startSlotArray[$j] .
+              ' - ' .
+              $endSlotArray[$j] .
+              '</option>';
+          }
+          
           foreach ($checkRoom as $room) {
             $query = $db->prepare(
-              'SELECT date, time FROM RESERVATION WHERE date = DATE(:date) AND time >= :startHour AND time <= :endHour AND id_activity = :id',
+              'SELECT id FROM RESERVATION WHERE date = DATE(:date) AND time >= :startHour AND time <= :endHour AND id_activity = :id',
             );
             $query->execute([
               'id' => $room['id'],
@@ -95,7 +106,8 @@ if (isset($_POST['id']) && isset($_POST['date']) && isset($_POST['day'])) {
             ]);
             $roomDate = $query->fetch(PDO::FETCH_ASSOC);
 
-            if ($roomDate === false) {
+
+            if (empty($roomDate) || empty($room)) {
               echo '<option value=' .
                 $startSlotArray[$j] .
                 '>' .
@@ -107,9 +119,20 @@ if (isset($_POST['id']) && isset($_POST['date']) && isset($_POST['day'])) {
           }
         }
       } elseif ($attendee <= $slot['maxAttendee']) {
+
+        if(empty($checkRoom)){
+          echo '<option value=' .
+            $startSlotArray[$j] .
+            '>' .
+            $startSlotArray[$j] .
+            ' - ' .
+            $endSlotArray[$j] .
+            '</option>';
+        }
+        
         foreach ($checkRoom as $room) {
           $query = $db->prepare(
-            'SELECT date, time FROM RESERVATION WHERE date = DATE(:date) AND time >= :startHour AND time <= :endHour AND id_activity = :id',
+            'SELECT id FROM RESERVATION WHERE date = DATE(:date) AND time >= :startHour AND time <= :endHour AND id_activity = :id',
           );
           $query->execute([
             'id' => $room['id'],
@@ -119,7 +142,7 @@ if (isset($_POST['id']) && isset($_POST['date']) && isset($_POST['day'])) {
           ]);
           $roomDate = $query->fetch(PDO::FETCH_ASSOC);
 
-          if ($roomDate === false) {
+          if (empty($roomDate))  {
             echo '<option value=' .
               $startSlotArray[$j] .
               '>' .
