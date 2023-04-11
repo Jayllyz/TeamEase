403c8bd16bbec3947ab2a32eb22f4ca5a9ed7e18
Simon <szhang0709@gmail.com>
2023-04-11 12:30:38 +0200

    add: jfreechart
    on vscode:
    ctrl + shift + p
    Java: Configure Classpath
    Referenced Libraries -> add : libs/jfreechart-1.5.4.jar

---
+++ src/dashboard/Dashboard.java
@@ -0,0 +1,33 @@
+package dashboard;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+public class Dashboard {
+    public static void main(String[] args) {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("Category 1", 43.2);
+        dataset.setValue("Category 2", 27.9);
+        dataset.setValue("Category 3", 79.5);
+
+        JFreeChart chart = ChartFactory.createPieChart(
+            "Dashboard",
+            dataset,
+            true,
+            true,
+            false
+        );
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+
+        JFrame frame = new JFrame("Dashboard");
+        frame.getContentPane().add(chartPanel, BorderLayout.CENTER);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
