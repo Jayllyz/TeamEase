f6e61f5927f28b9c4ceac1c082bc616bec759a24
Simon <szhang0709@gmail.com>
2023-04-20 12:47:56 +0200

    rm: useless import

--- src/dashboard/Dashboard.java
+++ src/dashboard/Dashboard.java
@@ -1,15 +1,11 @@
 package dashboard;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
