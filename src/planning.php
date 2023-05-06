c1cb89093432843937ea57cc337d21865a891f20
minatoco <minatoco@users.noreply.github.com>
2023-05-06 20:50:27 +0000

    Prettified Code!

--- src/planning.php
+++ src/planning.php
@@ -23,41 +23,49 @@ include 'includes/head.php';
     </div>
     <main>
         <?php
-            $sql = 'SELECT * FROM ROOM';
-            $stmt = $db->prepare($sql);
-            $stmt->execute();
-            $animates = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
-            for($b=0 ; $b < count($animates); $b++){
-            $sql = 'SELECT * FROM ROOM';
-            $stmt = $db->prepare($sql);
-            $stmt->execute();
-            $animatees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $sql = 'SELECT * FROM ROOM';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $animates = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            $sql = 'SELECT * FROM LOCATION WHERE id = :id';
-            $stmt = $db->prepare($sql);
-            $stmt->execute(['id' => $animates[$b]['id_location']]);
-            $locationzone = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        ?>
+        for ($b = 0; $b < count($animates); $b++) {
+
+          $sql = 'SELECT * FROM ROOM';
+          $stmt = $db->prepare($sql);
+          $stmt->execute();
+          $animatees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+          $sql = 'SELECT * FROM LOCATION WHERE id = :id';
+          $stmt = $db->prepare($sql);
+          $stmt->execute(['id' => $animates[$b]['id_location']]);
+          $locationzone = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          ?>
 
         <div class="container rounded main-text">
             <div class="container section-about-us border border-2 border-secondary rounded">
                 <div class="container rounded align-text-bottom">
                     <div class="row align-items-center">
                         <div class="col-8">
-                            <?php
-                            echo '<h5 class="row align-items-center">' . 'Nom de la salle : ' . $animates[$b]['name'] . '<br>' . $locationzone[0]['name'] . ' <br> ' .'Addresse : '  . $locationzone[0]['address'] . '</h5>';
-                             ?>
+                            <?php echo '<h5 class="row align-items-center">' .
+                              'Nom de la salle : ' .
+                              $animates[$b]['name'] .
+                              '<br>' .
+                              $locationzone[0]['name'] .
+                              ' <br> ' .
+                              'Addresse : ' .
+                              $locationzone[0]['address'] .
+                              '</h5>'; ?>
                         </div>
                     </div>
                 </div>
 
                 <?php if (isset($animates[$b]['id']) && !empty($animate[$b]['id'])) {
-              $sql = 'SELECT * FROM RESERVATION WHERE id_activity IN (SELECT id_activity FROM ACTIVITY WHERE id_room = :id_room)';
-              $stmt = $db->prepare($sql);
-              $stmt->execute(['id_room' => $animate[$b]['id']]);
-              $reservation = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            } ?>
+                  $sql =
+                    'SELECT * FROM RESERVATION WHERE id_activity IN (SELECT id_activity FROM ACTIVITY WHERE id_room = :id_room)';
+                  $stmt = $db->prepare($sql);
+                  $stmt->execute(['id_room' => $animate[$b]['id']]);
+                  $reservation = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } ?>
 
 
             <table style="border: 1px solid black;" class="table text-center table-bordered table-hover" id="active">
@@ -78,7 +86,8 @@ include 'includes/head.php';
                       $stmt->execute(['id_room' => $animatees[$b]['id']]);
                       $animate = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                      $sql ='SELECT * FROM RESERVATION WHERE id_activity IN (SELECT id_activity FROM ACTIVITY WHERE id_room = :id_room)';
+                      $sql =
+                        'SELECT * FROM RESERVATION WHERE id_activity IN (SELECT id_activity FROM ACTIVITY WHERE id_room = :id_room)';
                       $stmt = $db->prepare($sql);
                       $stmt->execute(['id_room' => $animatees[$b]['id']]);
                       $reservation = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -118,7 +127,6 @@ include 'includes/head.php';
                       $stmt->execute(['id_room' => $animatees[$b]['id']]);
                       $reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
                       echo '<td>';
                       for ($j = 0; $j < count($reservations); $j++) {
                         $configdate = explode('-', $reservations[$j]['date']);
@@ -128,14 +136,16 @@ include 'includes/head.php';
                         $stmt->execute(['id' => $reservations[$j]['id_activity']]);
                         $activities = $stmt->fetchAll(PDO::FETCH_ASSOC); // afficher les activités sans doublons
                         for ($k = 0; $k < count($reservations); $k++) {
-                            $sql = 'SELECT * FROM ACTIVITY WHERE id= :id';
-                            $stmt = $db->prepare($sql);
-                            $stmt->execute(['id' => $reservations[$k]['id_activity']]);
-                            $activity = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                            if (
-                                $datereservation == $dateplanning &&
-                                $reservations[$k]['time'] == $reservations[$j]['time'] && $reservations[$k]['id'] == $reservations[$j]['id'] && $activity[0]['id_room'] == $animatees[$b]['id']
-                            ) {
+                          $sql = 'SELECT * FROM ACTIVITY WHERE id= :id';
+                          $stmt = $db->prepare($sql);
+                          $stmt->execute(['id' => $reservations[$k]['id_activity']]);
+                          $activity = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                          if (
+                            $datereservation == $dateplanning &&
+                            $reservations[$k]['time'] == $reservations[$j]['time'] &&
+                            $reservations[$k]['id'] == $reservations[$j]['id'] &&
+                            $activity[0]['id_room'] == $animatees[$b]['id']
+                          ) {
                             //si l'activité est déjà affichée, on stoppe l'affichage
                             if (
                               $activity[0]['id'] == $activities[0]['id'] &&
@@ -162,24 +172,24 @@ include 'includes/head.php';
                           $duration = $stmt->fetch();
 
                           $sql = 'SELECT * FROM ACTIVITY WHERE id= :id';
-                            $stmt = $db->prepare($sql);
-                            $stmt->execute(['id' => $reservations[$k]['id_activity']]);
-                            $activity = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                          $stmt = $db->prepare($sql);
+                          $stmt->execute(['id' => $reservations[$k]['id_activity']]);
+                          $activity = $stmt->fetchAll(PDO::FETCH_ASSOC);
                           if (
                             $datereservation == $dateplanning &&
-                            $reservations[$k]['time'] == $reservations[$j]['time'] && $reservations[$k]['id'] == $reservations[$j]['id'] && $activity[0]['id_room'] == $animatees[$b]['id']
+                            $reservations[$k]['time'] == $reservations[$j]['time'] &&
+                            $reservations[$k]['id'] == $reservations[$j]['id'] &&
+                            $activity[0]['id_room'] == $animatees[$b]['id']
                           ) {
-                            
                             if (
                               $activity[0]['id'] == $activities[0]['id'] &&
                               $reservations[$k]['id'] != $reservations[$j]['id']
                             ) {
-                                var_dump($reservations[$k]);
-                                var_dump($reservations[$j]);
-                                exit;
+                              var_dump($reservations[$k]);
+                              var_dump($reservations[$j]);
+                              exit();
                               break;
                             } else {
-
                               $time = explode(':', $reservations[$k]['time']);
                               $time = $time[0] . 'h' . $time[1];
                               $duration = intval($duration[0]);
@@ -198,20 +208,22 @@ include 'includes/head.php';
                         }
                       }
                       echo '</td>';
-                    
+
                       echo '<td>';
                       for ($k = 0; $k < count($reservations); $k++) {
                         $configdate = explode('-', $reservations[$k]['date']);
                         $datereservation = $configdate[2] . '/' . $configdate[1] . '/' . $configdate[0];
 
                         $sql = 'SELECT * FROM ACTIVITY WHERE id= :id';
-                            $stmt = $db->prepare($sql);
-                            $stmt->execute(['id' => $reservations[$k]['id_activity']]);
-                            $activity = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                        $stmt = $db->prepare($sql);
+                        $stmt->execute(['id' => $reservations[$k]['id_activity']]);
+                        $activity = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         for ($j = 0; $j < count($reservations); $j++) {
                           if (
                             $datereservation == $dateplanning &&
-                            $reservations[$k]['time'] == $reservations[$j]['time'] && $reservations[$k]['id'] == $reservations[$j]['id'] && $activity[0]['id_room'] == $animatees[$b]['id']
+                            $reservations[$k]['time'] == $reservations[$j]['time'] &&
+                            $reservations[$k]['id'] == $reservations[$j]['id'] &&
+                            $activity[0]['id_room'] == $animatees[$b]['id']
                           ) {
                             if (
                               $activity[0]['id'] == $activities[0]['id'] &&
@@ -237,7 +249,9 @@ include 'includes/head.php';
             </table>
             </div>
         </div>
-        <?php } ?>
+        <?php
+        }
+        ?>
     </main>
     <?php include 'includes/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
