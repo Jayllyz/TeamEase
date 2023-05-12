cf5c6c7f7858d6c57fa48ade8dd670432e02fe02
Simon <szhang0709@gmail.com>
2023-05-12 16:58:33 +0200

    add: desgin

--- android/app/src/main/java/com/example/togetherstrongerapp/MainActivity.java
+++ android/app/src/main/java/com/example/togetherstrongerapp/MainActivity.java
@@ -106,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
                         startActivity(intent);
                     }
-                }, 200);
+                }, 1000);
             }
         });
     }
